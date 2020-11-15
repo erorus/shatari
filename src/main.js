e8efe8d40436c9c3407e225175034bdc36a9a8c3
Erorus <github@everynothing.net>
2020-11-15 12:51:11 -0500

    make globalstate global

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const ItemState = require('./itemState');
 const GlobalState = require('./globalState');
 
 const api = new BNet();
+let globalState;
 
 const MS_SEC = 1000;
 const MS_MINUTE = 60 * MS_SEC;
@@ -20,6 +21,8 @@ const realmQueue = {
 };
 
 async function main() {
+    globalState = await GlobalState.get();
+
     let realmIds = [4, 5, 9, 11, 12, 47, 52, 53, 54, 55, 57, 58, 60, 61, 63, 64, 67, 69, 71, 73, 75, 76, 77, 78, 84, 86, 96, 99, 100, 104, 106, 113, 114, 115, 117, 118, 120, 121, 125, 127, 151, 154, 155, 157, 158, 160, 162, 163, 1070, 1071, 1072, 1129, 1136, 1138, 1147, 1151, 1168, 1171, 1175, 1184, 1185, 1190, 1425, 1426, 1427, 1428, 3207, 3208, 3209, 3234, 3661, 3675, 3676, 3678, 3683, 3684, 3685, 3693, 3694, 3721, 3723, 3725, 3726];
 
     realmIds = realmIds.slice(10, 20);
@@ -279,7 +282,6 @@ async function processConnectedRealm(connectedRealmId) {
         });
 
         await GlobalState.lock();
-        const globalState = await GlobalState.get();
         globalState.snapshots = globalState.snapshots || {};
         globalState.snapshots[connectedRealmId] = thisSnapshot;
         await GlobalState.put(globalState);
