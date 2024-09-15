f81528083093e7c2e2ad23d8c5548a4e7e7ffbf1
Erorus <erorus@users.noreply.github.com>
2024-09-15 12:27:22 -0400

    only allow item variations for tww

--- src/addon.js
+++ src/addon.js
@@ -17,7 +17,7 @@ const CommodityRealm = require('./commodityRealm');
 
 const api = new BNet();
 const regions = [api.REGION_US, api.REGION_EU, api.REGION_TW, api.REGION_KR];
-const MIN_EXPANSION_ITEM_VARIATIONS = 10;
+const MIN_EXPANSION_ITEM_VARIATIONS = 11;
 const MEDIAN_PRICE_PERIOD = 4 * Constants.MS_DAY;
 
 let realmList;
